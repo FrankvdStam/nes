f7fb8fff00fcbfcf94095dad0c1d3a48d98d8d07
Frank v.d. Stam <frank_stam5@hotmail.com>
2019-10-22 20:36:42 +0200

    Added persistent state to debugwindow, values now persist beyond a single frame. Maybe update and render have to be merged - the way it is now, I can only pass events via render. Passing events via render and update isn't great. Only if I merged them will I have access to both window events and render events. In theory, they can never happen at the same time, if they end up doing that I will have to start returning a list of events.

--- src/rustnes/src/graphics/debugwindow.rs
+++ src/rustnes/src/graphics/debugwindow.rs
@@ -11,6 +11,15 @@ use glium::backend::glutin::glutin::{PossiblyCurrent, Event, ContextWrapper};
 pub enum DebugEvent
 {
     SetPixel { x: u32, y: u32, color: [f32; 3] },
+    CloseWindow
+}
+
+#[derive(Copy, Clone)]
+struct DebugWindowState
+{
+    x: u32,
+    y: u32,
+    color: [f32; 3],
 }
 
 
@@ -21,6 +30,9 @@ pub struct DebugWindow
     platform: WinitPlatform,
     renderer: Renderer,
     events_loop: glium::glutin::EventsLoop,
+
+    //Holds state of the gui
+    state: DebugWindowState,
 }
 
 impl DebugWindow
@@ -50,6 +62,13 @@ impl DebugWindow
             renderer,
             platform,
             events_loop,
+
+            state: DebugWindowState
+            {
+                x: 0,
+                y: 0,
+                color: [0.0,0.0,0.0],
+            }
         }
     }
 
@@ -68,7 +87,6 @@ impl DebugWindow
 
     pub fn render(&mut self) -> Option<DebugEvent>
     {
-
         let mut event: Option<DebugEvent> = None;
 
         let mut frame = self.display.draw();
@@ -80,6 +98,7 @@ impl DebugWindow
         let io = self.imgui.io_mut();
         self.platform.prepare_frame(io, &window).expect("Failed to start frame");
         let mut ui = self.imgui.frame();
+        let mut state = self.state.clone();
 
         Window::new(im_str!("Hello world"))
             .size([230.0, 800.0], Condition::Always)
@@ -87,19 +106,18 @@ impl DebugWindow
             .build(&ui, || {
                 let mut demo = true;
                 ui.show_demo_window(&mut demo);
-                let mut x = 0;
-                let mut y = 0;
-                let mut color: [f32; 3] = [0.0, 0.0, 0.0];
-                Slider::new(im_str!("x"), (0..=9)).build(&ui, &mut x);
-                Slider::new(im_str!("y"), (0..=9)).build(&ui, &mut y);
-                ColorPicker::new(im_str!("Color"), &mut color).build(&ui);
+
+                Slider::new(im_str!("x"), (0..=9)).build(&ui, &mut state.x);
+                Slider::new(im_str!("y"), (0..=9)).build(&ui, &mut state.y);
+                ColorPicker::new(im_str!("Color"), &mut state.color).build(&ui);
                 if ui.button(im_str!("Set"), [100.0,20.0])
                 {
-                    event = Some(DebugEvent::SetPixel{ x, y, color});
-                    //canvas.set_color(x as u32, y as u32, color);
-                    //canvas.refresh_vertex_buffer(&display);
+                    event = Some(DebugEvent::SetPixel{ x: state.x, y: state.y, color: state.color});
                 }
+
             });
+
+        self.state = state;
 
         self.platform.prepare_render(&ui, &window);
         let draw_data = ui.render();
